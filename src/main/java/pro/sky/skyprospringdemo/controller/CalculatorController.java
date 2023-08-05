9971f4a0fdf50e57c7e6f20d815b988f7f4f6345
Azych1 <nagimov123@gmail.com>
2023-08-06 00:13:14 +0500

    Домашка решена, решил вопрос с ошибкой при отсутствии параметров в запросе

--- src/main/java/pro/sky/skyprospringdemo/controller/CalculatorController.java
+++ src/main/java/pro/sky/skyprospringdemo/controller/CalculatorController.java
@@ -34,16 +34,14 @@ public class CalculatorController {
     }
 
     @GetMapping("/divide")
-    public String divide(@RequestParam int num1,@RequestParam int num2) {
-        if (num2 == 0) {
-            return "Деление на 0 не доступно";
+    public String divide(@RequestParam (required = false) Integer num1,@RequestParam (required = false) Integer num2) {
+        if (num1 != null && num2 != null) {
+            if (num2 == 0) {
+                return "Деление на 0 не доступно";
+            }
+        } else {
+            return "Не хватает числа(-ел) для выполнения операции";
         }
-//          !! Не понял как вывести ошибку если параметры не заданы. Через Integer?
-
-
-//        else if (num1==null||num2==null) {
-//            return "Не хватает числа(-ел) для выполнения операции";
-//        }
 
         return calculatorService.divide(num1, num2);
     }
--- src/main/java/pro/sky/skyprospringdemo/service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/skyprospringdemo/service/CalculatorServiceImpl.java
@@ -28,7 +28,7 @@ public class CalculatorServiceImpl implements CalculatorService {
     }
 
     @Override
-    public String divide(int num1, int num2) {
+    public String divide(Integer num1, Integer num2) {
         double result = (double) num1 / num2;
         return num1 + " / " + num2 + " = " + result;
     }
